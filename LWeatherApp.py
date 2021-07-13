95612eb958720192ff522da16717f8415541c691
Tanner Coker <tbcoker13@gmail.com>
2021-07-13 15:45:39 -0500

    Began to add input options for choosing location

--- LWeatherApp.py
+++ LWeatherApp.py
@@ -16,16 +16,25 @@ params = {
     'key': API_KEY,
 }
 
-
 def getLocation():
+    UseIP = StringVar()
+    IPcheck = Checkbutton(frame, text='Get weather via IP', variable=UseIP, onvalue='True',offvalue='False', font=('Helvetica',12))
+    
+    cityLbl = Label(frame, text='City:', fg='black', font=('Helvetica',12))
+    IPcheck.place(x=50,y=50)
+    cityLbl.place(x=50,y=100)
+
+def checkInput():
     pass
 
 def showWeather():
     pass
 
 root = Tk()
+root.title('LWeatherApp')
 #lwa = LWeatherApp(root)
 root.geometry('400x500')
 frame = Frame(root)
-frame.pack()
+frame.pack(side="top",expand=True,fill='both')
+getLocation()
 root.mainloop()
